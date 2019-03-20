43130deb9cf5c1c89caad710cd1ae418050c7485
nhadziosma1 <nhadziosma1@etf.unsa.ba>
2019-03-20 16:14:02 +0100

    - nekiString.Split(char[]) - nekiString = String.Concat(char[]) - nekiString.toLower()
    - za stringove se, kao i u Javi, koristi "equals", umjesto usporedbe na jendoakost

---
+++ ooad-t1-z5/ooad-t1-z5/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace ooad_t1_z5
+{
+    class Program
+    {
+        public static bool daLiJePalindrom(String unos)
+        {
+            char[] zanemariviZankovi = { ' ', ',', '.', '!', '?', ';', ':' };
+            string[] podstringovi = unos.Split(zanemariviZankovi);
+            unos= String.Concat(podstringovi).ToLower();
+
+            int pocetak = 0, kraj = unos.Length-1;
+            while (pocetak < kraj)
+            {
+                char prvi, zadnji;
+
+                prvi = unos[pocetak];
+                zadnji = unos[kraj];
+
+                if (prvi.Equals(zadnji) == false)
+                    return false;
+
+                pocetak++;
+                kraj--;
+            }
+
+            return true;
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Unesite izrez/recenicu, da bi provjerili da li se radi o palindromu: ");
+            String unos = Console.ReadLine();
+            Console.WriteLine("Uneseni izaz/recenica {0} palindrom", daLiJePalindrom(unos));
+
+            Console.ReadLine();
+        }
+    }
+}
